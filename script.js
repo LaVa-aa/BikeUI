c06319d0c6961b3739b2e6bece8d3b3e2c37ce5f
Lava Maamo <lama26830@edu.ucl.dk>
2022-05-29 12:54:15 +0200

    opsætning

---
+++ script.js
@@ -0,0 +1,14 @@
+ const localUrl = "https://restbike.azurewebsites.net/api/Bikes"
+
+// app oprettes
+const app = Vue.createApp({
+    data() { // appens værdier defineres
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+
+}).mount("#app") //her bliver appen mounted
